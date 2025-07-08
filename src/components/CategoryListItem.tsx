d7d6ff997339109d3be9f6f041385a6b39402031
Nadeem Iqbal <nadeem+1@nextgeni.com>
2025-07-08 11:43:26 +0500

    fix: use user-provided, highly relevant images for all main categories

--- src/components/CategoryListItem.tsx
+++ src/components/CategoryListItem.tsx
@@ -18,13 +18,13 @@ export type CategoryListItemType = {
 
 const categoryImages: Record<string, string> = {
   'Hot Deals': 'https://cdn-icons-png.flaticon.com/512/616/616490.png',
-  'iPhone': 'https://cdn-icons-png.flaticon.com/512/747/747376.png',
-  'Samsung': 'https://cdn-icons-png.flaticon.com/512/882/882704.png',
-  'Macbooks': 'https://cdn-icons-png.flaticon.com/512/1055/1055687.png',
-  'Laptops': 'https://cdn-icons-png.flaticon.com/512/337/337946.png',
-  'iPads': 'https://cdn-icons-png.flaticon.com/512/747/747376.png',
-  'Apple Watches': 'https://cdn-icons-png.flaticon.com/512/3103/3103446.png',
-  'Accessories': 'https://cdn-icons-png.flaticon.com/512/1042/1042330.png',
+  'iPhone': 'https://iplanet.one/cdn/shop/files/iPhone_14_Blue_PDP_Image_Position-1A__WWEN.jpg?v=1691142210&width=1920',
+  'Samsung': 'https://bsmedia.business-standard.com/_media/bs/img/about-page/1562575696.png',
+  'Macbooks': 'https://www.apple.com/newsroom/images/product/mac/standard/Apple-MacBook-Pro-M2-Pro-and-M2-Max-hero-230117.jpg.landing-big_2x.jpg',
+  'Laptops': 'https://blog.techbazaar.pk/wp-content/uploads/2024/09/budget-laptop-2.webp',
+  'iPads': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNYhoEMalf2MG3C7UYYs_MrCa7Yow32Nj3yu-1Sl8d0dqykoOge-nYHHOhRkuyeHmL9mw&usqp=CAU',
+  'Apple Watches': 'https://www.apple.com/newsroom/images/product/watch/standard/Apple_watch-experience-for-entire-family-hero_09152020_big.jpg.large.jpg',
+  'Accessories': 'https://mate.net.in/public/uploads/all/UsReqZvujmEjMUb27qlTtRcCG8Pf18SyULO4HW7U.jpg',
   'Gaming': 'https://cdn-icons-png.flaticon.com/512/1055/1055687.png',
 };
 
